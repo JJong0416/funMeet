bb3e47594d7768b2eacc8a17bc2a03af1f2d1de8
jjong0416-tech <whdcks5681@naver.com>
2021-07-18 13:32:33 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/funmeet/FunMeet.java
@@ -0,0 +1,13 @@
+package com.funmeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunMeet {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunMeet.class, args);
+    }
+
+}
---
+++ src/test/java/com/funmeet/FunMeetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.funmeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunMeetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
