91f330be26c27c4866ad897db9edf0551254be3e
jjong0416-tech <whdcks5681@naver.com>
2021-07-19 21:10:21 +0900

    feat: 닉네임 & 이메일 검증

--- src/main/java/com/funmeet/controller/AccountController.java
+++ src/main/java/com/funmeet/controller/AccountController.java
@@ -1,17 +1,32 @@
 package com.funmeet.controller;
 
 import com.funmeet.form.SignUpForm;
+import com.funmeet.repository.AccountRepository;
+import com.funmeet.validator.SignUpFormValidator;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
 
 @Controller
+@RequiredArgsConstructor
 public class AccountController {
+
+    private final AccountRepository accountRepository;
+    private final SignUpFormValidator signUpFormValidator;
+
+    //TODO SignUp Validation Check
+    @InitBinder("signUpForm")
+    public void initBinder(WebDataBinder webDataBinder){
+        webDataBinder.addValidators(signUpFormValidator);
+    }
 
     @GetMapping({"","/"})
     public String Home(){
---
+++ src/main/java/com/funmeet/validator/SignUpFormValidator.java
@@ -0,0 +1,35 @@
+package com.funmeet.validator;
+
+import com.funmeet.form.SignUpForm;
+import com.funmeet.repository.AccountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+
+@Component
+@RequiredArgsConstructor
+public class SignUpFormValidator implements Validator {
+
+    private final AccountRepository accountRepository;
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return clazz.isAssignableFrom(SignUpForm.class);
+    }
+
+    @Override
+    // TODO validation email, nickname
+    public void validate(Object target, Errors errors) {
+        SignUpForm signUpForm = (SignUpForm) target;
+
+        if(accountRepository.existsByNickname(signUpForm.getNickname())){
+            errors.rejectValue("nickname","invalid nickname",new Object[]{signUpForm.getNickname()},"이미 사용중인 아이디입니다.");
+        }
+
+        if (accountRepository.existsByEmail(signUpForm.getEmail())){
+            errors.rejectValue("email","invalid.email",new Object[]{signUpForm.getEmail()},"이미 사용중인 이메일입니다.");
+        }
+    }
+}
