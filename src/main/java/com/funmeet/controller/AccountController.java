8653f49134143226821cf528f8adfcbc88dec6b7
jjong0416-tech <whdcks5681@naver.com>
2021-07-19 20:12:27 +0900

    feat: 회원가입 폼

--- src/main/java/com/funmeet/controller/AccountController.java
+++ src/main/java/com/funmeet/controller/AccountController.java
@@ -1,19 +1,34 @@
 package com.funmeet.controller;
 
+import com.funmeet.form.SignUpForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class AccountController {
 
     @GetMapping({"","/"})
-    public String Home(Model model){
+    public String Home(){
         return "index";
     }
 
     @GetMapping("/sign-up")
     public String signUpForm(Model model){
+        model.addAttribute("signupForm",new SignUpForm());
         return "account/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid @ModelAttribute SignUpForm signUpForm, Errors errors){
+        if (errors.hasErrors()){
+            return "account/sign-up";
+        }
+        return "redirect:/";
     }
 }
---
+++ src/test/java/com/funmeet/controller/AccountControllerTest.java
@@ -0,0 +1,33 @@
+package com.funmeet.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AccountControllerTest {
+
+    @Autowired private MockMvc mockMvc;
+
+    @DisplayName("회원 가입 화면 보이는지 테스트")
+    @Test
+    void signUpForm(){
+        try {
+            mockMvc.perform(get("/sign-up"))
+                    .andExpect(status().isOk()) // 정상처리 된지
+                    .andExpect(view().name("account/sign-up"))
+                    .andExpect(model().attributeExists("signUpForm"));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
