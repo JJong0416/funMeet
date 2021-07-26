e90b6b1884ce69df077395e98549330a3ae85ccb
jjong0416-tech <whdcks5681@naver.com>
2021-07-26 16:24:42 +0900

    refactoring: 로그인 테스트 작성

---
+++ src/test/java/com/funmeet/controller/HomeControllerTest.java
@@ -0,0 +1,95 @@
+package com.funmeet.controller;
+
+import com.funmeet.form.SignUpForm;
+import com.funmeet.repository.AccountRepository;
+import com.funmeet.service.AccountService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+class HomeControllerTest {
+
+    @Autowired MockMvc mockMvc;
+    @Autowired
+    AccountService accountService;
+    @Autowired
+    AccountRepository accountRepository;
+
+
+    @BeforeEach
+    void beforeEach(){
+        SignUpForm signUpForm = new SignUpForm();
+        signUpForm.setNickname("jjong1234");
+        signUpForm.setPassword("12345678");
+        signUpForm.setEmail("jjong@gmail.com");
+
+        accountService.processSignUpAccount(signUpForm);
+    }
+
+    @AfterEach
+    void afterEach(){
+        accountRepository.deleteAll();
+    }
+
+
+    @DisplayName("이메일로 로그인")
+    @Test
+    void login_with_email() throws Exception{
+        mockMvc.perform(post("/login")
+                .param("username","jjong@gmail.com")
+                .param("password","12345678")
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"))
+                .andExpect(authenticated().withUsername("jjong1234"));
+    }
+
+    @DisplayName("닉네임으로 로그인")
+    @Test
+    void login_with_nickname() throws Exception{
+        mockMvc.perform(post("/login")
+                .param("username","jjong1234")
+                .param("password","12345678")
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"))
+                .andExpect(authenticated().withUsername("jjong1234"));
+    }
+
+    @DisplayName("로그인 실패")
+    @Test
+    void login_fail() throws Exception{
+        mockMvc.perform(post("/login")
+                .param("username","jjong0416@gmail.com")
+                .param("password","12345678")
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login?error"));
+    }
+
+    @DisplayName("로그아웃")
+    @Test
+    void logout() throws Exception{
+        mockMvc.perform(post("/logout")
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"))
+                .andExpect(unauthenticated());
+    }
+}
