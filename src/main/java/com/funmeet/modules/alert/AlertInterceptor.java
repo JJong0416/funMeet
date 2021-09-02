0014e191d4997b973955bee5402881a501b8e40f
jjong0416-tech <whdcks5681@naver.com>
2021-09-02 23:57:16 +0900

    Feat(Alert): 모임 생성 홍보 메세지

---
+++ src/main/java/com/funmeet/modules/alert/AlertInterceptor.java
@@ -0,0 +1,38 @@
+package com.funmeet.modules.alert;
+
+import com.funmeet.modules.account.Account;
+import com.funmeet.modules.account.AdaptAccount;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+@RequiredArgsConstructor
+public class AlertInterceptor implements HandlerInterceptor {
+
+    private final AlertRepository alertRepository;
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws  Exception {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (modelAndView != null && !isRedirectView(modelAndView) && authentication != null && authentication.getPrincipal() instanceof AdaptAccount) {
+            Account account = ((AdaptAccount)authentication.getPrincipal()).getAccount();
+            long count = alertRepository.countByAccountAndChecked(account, false);
+            modelAndView.addObject("alertMessage", count > 0);
+        }
+    }
+
+    private boolean isRedirectView(ModelAndView modelAndView) {
+        return modelAndView.getViewName().startsWith("redirect:") || modelAndView.getView() instanceof RedirectView;
+    }
+
+}
--- src/main/java/com/funmeet/modules/alert/AlertRepository.java
+++ src/main/java/com/funmeet/modules/alert/AlertRepository.java
@@ -1,6 +1,8 @@
 package com.funmeet.modules.alert;
 
+import com.funmeet.modules.account.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AlertRepository extends JpaRepository<Alert, Long> {
+    long countByAccountAndChecked(Account account, boolean check);
 }
