526c1130d282a40d788f84104b49f676a2e66c61
jjong0416-tech <whdcks5681@naver.com>
2021-09-09 15:26:22 +0900

    Feat(Remember-Me): 리멤버미 구현

--- src/main/java/com/funmeet/infra/config/SecurityConfig.java
+++ src/main/java/com/funmeet/infra/config/SecurityConfig.java
@@ -1,17 +1,29 @@
 package com.funmeet.infra.config;
 
+import com.funmeet.modules.account.AccountService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final DataSource dataSource;
+    private final UserDetailsService userDetailsService;
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.authorizeRequests()
@@ -23,8 +35,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.formLogin()
                 .loginPage("/login").permitAll();
 
+        http.rememberMe()
+                .userDetailsService(userDetailsService)
+                .tokenRepository(persistentTokenRepository());
+
         http.logout()
                 .logoutSuccessUrl("/");
+    }
+
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository(){
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setDataSource(dataSource);
+
+        return jdbcTokenRepository;
     }
 
     @Override
